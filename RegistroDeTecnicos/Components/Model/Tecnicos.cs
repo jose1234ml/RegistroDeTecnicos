189fbebdca5bd447028c656b87471312cd7445e9
macbookair <macbookair@MACBOOKAIRDCC0>
2025-05-15 19:47:23 -0400

    Clase tecnicos modelo terminada

---
+++ RegistroDeTecnicos/Components/Model/Tecnicos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroDeTecnicos.Components.Model
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+
+        [Required(ErrorMessage ="Este campo es obligatorio")] 
+        public string NombreTecnico { get; set; } = null!;
+        [Range(0.1, double.MaxValue, ErrorMessage = "Este campo es obligatorio y debe ser mayor que 0")]
+        public double SueldoHora { get; set; }
+
+    }
+}
