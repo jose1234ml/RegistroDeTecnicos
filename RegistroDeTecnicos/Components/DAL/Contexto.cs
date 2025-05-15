fca1eb1be42fca918ca32e1d0d81152f26308e85
macbookair <macbookair@MACBOOKAIRDCC0>
2025-05-15 19:56:43 -0400

    Contexto Terminado

---
+++ RegistroDeTecnicos/Components/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroDeTecnicos.Components.Model;
+
+namespace RegistroDeTecnicos.Components.DAL
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options): base(options) { }
+
+        public DbSet<Tecnicos> Tecnicos { get; set; }
+    }
+}
