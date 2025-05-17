375a6f36b26a6794bbbfc89b6afb1c75b18cf9e3
macbookair <macbookair@MACBOOKAIRDCC0>
2025-05-17 18:17:37 -0400

    mover clase tecnico creada en carpeta equivocada y correccion del codigo

---
+++ RegistroDeTecnicos/Components/Model/Tecnico.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroDeTecnicos.Components.Model
+{
+    public class Tecnico
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+
+        // El nombre no debe contener números y es requerido
+        [Required(ErrorMessage = "Debe ingresar el nombre del técnico")]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "El nombre no puede contener números")]
+        public string NombreTecnico { get; set; } = string.Empty;
+
+        // El sueldo por hora debe ser mayor que 0 y puede tener hasta 2 decimales
+        [Range(0.1, 50000, ErrorMessage = "El sueldo debe ser mayor que 0 y menor o igual a 50,000")]
+        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "El formato debe ser numérico con hasta dos decimales")]
+        public double SueldoHora { get; set; }
+    }
+}
