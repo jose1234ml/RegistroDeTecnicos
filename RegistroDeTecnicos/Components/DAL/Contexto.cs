5302e56a14380080c2d92afa4a6166091b9d5400
macbookair <macbookair@MACBOOKAIRDCC0>
2025-05-23 09:44:17 -0400

    Modica clase contexto para agragar cliente

--- RegistroDeTecnicos/Components/DAL/Contexto.cs
+++ RegistroDeTecnicos/Components/DAL/Contexto.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RegistroDeTecnicos.Components.Model;
 
-namespace RegistroDeTecnicos.Components.DAL
+namespace RegistroDeTecnicos.Components.DAL;
+public class Contexto : DbContext
 {
-    public class Contexto: DbContext
-    {
-        public Contexto(DbContextOptions<Contexto> options): base(options) { }
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public DbSet<Tecnicos> Tecnicos { get; set; }
+    public DbSet<Cliente> Clientes { get; set; }
 
-        public DbSet<Tecnicos> Tecnicos { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Cliente>()
+            .HasIndex(c => c.Nombres)
+            .IsUnique();
+
+        modelBuilder.Entity<Cliente>()
+            .HasIndex(c => c.Rnc)
+            .IsUnique();
+
+        modelBuilder.Entity<Cliente>()
+            .HasOne(c => c.Tecnico)
+            .WithMany()
+            .HasForeignKey(c => c.TecnicoId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
