0926587dfc2634f286e52b25c3e8cc92bad74e51
Malvin <Malvinjose16@gmail.com>
2025-05-29 23:21:51 -0400

    Agerga clase Ticket

--- RegistroDeTecnicos/Components/Model/Ticket.cs
+++ RegistroDeTecnicos/Components/Model/Ticket.cs
@@ -2,5 +2,18 @@
 {
     public class Ticket
     {
+        public int TicketId { get; set; }
+        public string Asunto { get; set; }
+        public string Descripcion { get; set; }
+        public string Prioridad { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public int TiempoInvertido { get; set; } 
+
+    public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+
+        public int TecnicoId { get; set; }
+        public Tecnicos Tecnico { get; set; }
     }
 }
